4ce37b6a9cedff8a5f00c789014438be871b8442
Aleksandr Zhuk <taradesolokarel@mail.ru>
2021-07-29 12:55:47 +0300

    add branch

---
+++ Lesson7/Cat.java
@@ -0,0 +1,27 @@
+package Javatests.Lesson7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+      public Cat(String name, int appetite ) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    public void eat(Plate plate) {
+            plate.decreaseFood(appetite,name);
+    }
+
+
+}
